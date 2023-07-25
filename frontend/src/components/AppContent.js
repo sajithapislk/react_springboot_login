e99aebefabab8bd74dc858aaf324d6fa04f40c5a
sajithapislk <sajith@apis.lk>
2023-07-25 17:09:33 +0530

    Update AppContent.js

--- frontend/src/components/AppContent.js
+++ frontend/src/components/AppContent.js
@@ -1,12 +1,6 @@
 import * as React from "react";
 
 export default class AppContent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      componentToShow: "welcome",
-    };
-  }
   render() {
     return (
       <>
