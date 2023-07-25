59618717b34c9b0fcea812fa72067f035648f658
sajithapislk <sajith@apis.lk>
2023-07-25 22:37:21 +0530

    update

--- backend/src/main/java/lk/apis/login/backend/controller/MessagesController.java
+++ backend/src/main/java/lk/apis/login/backend/controller/MessagesController.java
@@ -1,4 +1,4 @@
-package lk.apis.login.backend.Controller;
+package lk.apis.login.backend.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
